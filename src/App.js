f6caa7e0cbffd6f250abfad66b55736a13d1a24f
Santhosh <santhoshmani30@gmail.com>
2020-02-11 21:21:14 +0200

    updated

--- src/App.js
+++ src/App.js
@@ -1,24 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import { Grid, TextField } from '@material-ui/core';
+import axios from 'axios';
 import './App.css';
 
-function App() {
+
+const API = axios.create({});
+
+const App = () => {
+  const [source, setSource] = useState('');
+  const [value, setValue] = useState('');
+  const [dest, setDest] = useState('');
+  const [currencies, setCurrencies] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      let res = await API.get('/currency');
+      setCurrencies(res.data)
+      console.log(res);
+    };
+    fetchData();
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Grid container alignItems="center" justify="center" style={{ height: '100vh' }}>
+        <Grid item style={{ padding: 5 }}>
+          <TextField
+            style={{ width: 150 }}
+            variant="outlined"
+            id="app-currencyValue"
+            type="number"
+            size="small"
+            label="Amount"
+            value={value}
+            onChange={e => setValue(e.target.value)}
+          ></TextField>
+        </Grid>
+
+        <Grid item style={{ padding: 5 }}>
+          <TextField
+            style={{ width: 150 }}
+            variant="outlined"
+            id="app-sourceCurrency"
+            size="small"
+            label="From"
+            value={source}
+            onChange={e => setSource(e.target.value)}
+          ></TextField>
+        </Grid>
+
+        <Grid item style={{ padding: 5 }}>
+          <TextField
+            style={{ width: 150 }}
+            variant="outlined"
+            id="app-destCurrency"
+            size="small"
+            label="To"
+            value={dest}
+            onChange={e => setDest(e.target.value)}
+          ></TextField>
+        </Grid>
+      </Grid>
     </div>
   );
 }
